fe27bce96fdb088d9b6aef6824ace5322b8a0221
tomershoham10 <tomershoham10@gmail.com>
2023-07-26 08:51:51 +0300

    fixing search button css x2

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -20,7 +20,7 @@ const NavBar = () => {
       <div
         className={`${
           !isClicked
-            ? "flex justify-start items-center rounded-md transition-all ease-out bg-transparent duration-700"
+            ? "flex justify-start items-center rounded-md transition-all ease-out bg-transparent duration-500"
             : "flex justify-start items-center pt-2 pr-2 pb-2 rounded-md bg-white transition-all ease-in-out duration-700"
         }`}
       >
@@ -28,14 +28,12 @@ const NavBar = () => {
           type="text"
           className={`${
             !isClicked
-              ? "invisible bg-transparent max-w-none w-0 transition-all ease-in duration-500 outline-none"
+              ? "invisible bg-transparent max-w-none w-0 transition-all ease-in duration-700 outline-none"
               : "w-24 bg-transparent text-[#5A4A42] transition-all ease-in-out duration-700 outline-none"
           }`}
         />
         <button
-          className={`${
-            !isClicked ? "hover:delay-300 nav-items" : "search-button"
-          }`}
+          className={`${!isClicked ? "nav-items" : "search-button"}`}
           onClick={() => searchClick()}
         >
           <BiSearchAlt />

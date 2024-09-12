50816e830a9b61980b72f76e02f80d8f86bb00a9
tomershoham10 <tomershoham10@gmail.com>
2024-09-13 00:21:12 +0300

    feat: Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+interface ButtonProps {
+  label: string;
+  className?: string;
+  onClick: () => void;
+}
+const Button: React.FC<ButtonProps> = (props) => {
+  const { label, className, onClick } = props;
+  return (
+    <button
+      className={`h-12 w-24 rounded-lg bg-recipeGreen-default text-xl font-bold text-recipeGray-lightest ${className}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;

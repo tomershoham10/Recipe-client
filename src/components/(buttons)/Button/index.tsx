a210d432e640550299981c12e1fc47928c7a7dfb
tomershoham10 <tomershoham10@gmail.com>
2024-09-15 11:38:07 +0300

    style: bg colors

--- src/components/(buttons)/Button/index.tsx
+++ src/components/(buttons)/Button/index.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<ButtonProps> = (props) => {
   const { label, className, onClick } = props;
   return (
     <button
-      className={`h-12 w-24 rounded-lg bg-recipeGreen-default text-xl font-bold text-recipeGray-lightest ${className}`}
+      className={`h-12 w-24 rounded-lg bg-recipeBlue-default text-xl font-bold text-recipeGray-lightest ${className}`}
       onClick={onClick}
     >
       {label}
--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -13,7 +13,7 @@ const Chips: React.FC<ChipsProps> = (props) => {
       {values.map((label, index) => (
         <div
           key={index}
-          className='flex w-fit flex-row gap-2 rounded-3xl bg-recipeGreen-default pr-2 pl-3 py-1 text-lg text-recipeGray-lightest'
+          className='flex w-fit flex-row gap-2 rounded-3xl bg-recipeBlue-default pr-2 pl-3 py-1 text-lg text-recipeGray-lightest'
         >
           <button onClick={() => onClick(index)}>
             <FaXmark />

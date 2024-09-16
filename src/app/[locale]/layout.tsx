0fa5adfd0cdfd1f911a1e720a92ef2b8a843efd4
tomershoham10 <tomershoham10@gmail.com>
2024-09-16 11:20:19 +0300

    refactor: transfered NextIntlClientProvider to the root layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,22 +1,14 @@
-import { NextIntlClientProvider, useMessages } from 'next-intl';
 import CreateNewIngredient from '../(popups)/(create)/CreateNewIngredient';
 
 export default function LocaleLayout({
   children,
-  params: { locale },
 }: {
   children: React.ReactNode;
-  params: { locale: string };
 }) {
-  // ...
-
-  // Receive messages provided in `i18n.ts`
-  const messages = useMessages();
-
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
+    <section>
       <CreateNewIngredient />
       {children}
-    </NextIntlClientProvider>
+    </section>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Assistant } from 'next/font/google';
-import NavBar from '@/components/NavBar/page';
-// import CreateNewIngredient from "./_popups/CreateNewIngredient/page";
+
+import NavBar from '@/components/NavBar';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
 
 const assistant = Assistant({ subsets: ['latin'] });
 
@@ -12,18 +13,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params: { locale },
 }: {
   children: React.ReactNode;
+  params: { locale: string };
 }) {
+  const messages = useMessages();
+
   return (
     <html lang={'he'} dir='rtl'>
       <body
         suppressHydrationWarning={true}
-        className={`${assistant.className} text-recipeBrown-dark flex flex-col bg-recipeGray-light`}
+        className={`${assistant.className} flex flex-col bg-recipeGray-light text-recipeBrown-dark`}
       >
-        {/* <CreateNewIngredient /> */}
-        <NavBar/>
-        <section className='h-full text-recipeGray-darker overflow-hidden'>{children}</section>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <NavBar />
+          <section className='h-full overflow-hidden text-recipeGray-darker'>
+            {children}
+          </section>
+        </NextIntlClientProvider>
       </body>
     </html>
   );

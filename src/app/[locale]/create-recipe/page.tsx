49b435a5b21903d602852166a987e143eca7a5b1
tomershoham10 <tomershoham10@gmail.com>
2024-09-19 16:12:41 +0300

    feat: adding ingredients works

--- src/app/[locale]/create-recipe/page.tsx
+++ src/app/[locale]/create-recipe/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useCallback, useEffect, useReducer } from 'react';
+import { useCallback, useEffect, useMemo, useReducer } from 'react';
 import { useTranslations } from 'next-intl';
 
 import { FaXmark } from 'react-icons/fa6';
@@ -15,9 +15,19 @@ import {
   createRecipeReducer,
 } from '@/reducers/createRecipeReducer';
 import { Units } from '@/app/API/recipe-service/ingredients/functions';
+import { useFetchIngredients } from '@/app/utils/hooks/useFetchIngredients';
 
 const CreateRecipe: React.FC = () => {
-  const t = useTranslations('createRecipe');
+  const tCreateRecipe = useTranslations('createRecipe');
+  const tUnits = useTranslations('units');
+
+  const ingredientsList = useFetchIngredients();
+
+  const sortedIngredientsList = useMemo(() => {
+    return ingredientsList?.sort((a, b) => a.name.localeCompare(b.name)) || [];
+  }, [ingredientsList]);
+
+  console.log('ingredientsListingredientsList', ingredientsList);
 
   const initialCreateRecipeState = {
     name: '',
@@ -32,7 +42,7 @@ const CreateRecipe: React.FC = () => {
     ],
 
     newIngredientsBySection: {
-      0: { ingredientId: '', unit: Units.UNITS, index: 0, quantity: 0 },
+      0: { ingredientId: '', unit: tUnits(Units.UNITS), index: 0, quantity: 0 },
     },
 
     steps: [],
@@ -68,10 +78,12 @@ const CreateRecipe: React.FC = () => {
   return (
     <section className='flex h-full w-full flex-row'>
       <section className='flex w-full basis-1/2 flex-col gap-3 border-l-2 pr-3 pt-4'>
-        <p className='pr-4 text-5xl font-bold'>{t('createRecipe')}</p>
-        <section className='mr-4 w-4/5 rounded-2xl bg-white px-4 py-2'>
+        <p className='pr-4 text-5xl font-bold'>
+          {tCreateRecipe('createRecipe')}
+        </p>
+        <section className='mr-4 w-4/5 rounded-2xl bg-recipeGray-lightest px-4 py-2'>
           <p className='mb-1 text-xl font-semibold opacity-80'>
-            {t('addRecipeName')}
+            {tCreateRecipe('addRecipeName')}
           </p>
           <Input
             type={InputTypes.TEXT}
@@ -85,9 +97,9 @@ const CreateRecipe: React.FC = () => {
           />
         </section>
 
-        <section className='mr-4 w-4/5 rounded-2xl bg-white px-4 py-2'>
+        <section className='mr-4 w-4/5 rounded-2xl bg-recipeGray-lightest px-4 py-2'>
           <p className='mb-1 text-xl font-semibold opacity-80'>
-            {t('addRecipeDescription')}
+            {tCreateRecipe('addRecipeDescription')}
           </p>
           <Textbox
             isEditMode={false}
@@ -102,9 +114,9 @@ const CreateRecipe: React.FC = () => {
           />
         </section>
 
-        <section className='mr-4 w-4/5 rounded-2xl bg-white px-4 py-2'>
+        <section className='mr-4 w-4/5 rounded-2xl bg-recipeGray-lightest px-4 py-2'>
           <p className='mb-1 text-xl font-semibold opacity-80'>
-            {t('uploadRecipePictue')}
+            {tCreateRecipe('uploadRecipePictue')}
           </p>
           <Dropzone
             isMultiple={false}
@@ -126,20 +138,22 @@ const CreateRecipe: React.FC = () => {
           )}
         </section>
       </section>
-      <section className='h-full w-full basis-1/2 overflow-y-auto py-4 pr-3'>
-        <section className='relative mr-4 h-fit w-4/5 rounded-2xl bg-white px-4 pb-7 pt-2'>
-          <p className='mb-1 text-3xl font-bold'>{t('addRecipeIngredients')}</p>
+      <section className='flex h-full w-full basis-1/2 flex-col gap-3 overflow-y-auto py-4 pr-3'>
+        <section className='relative mr-4 h-fit w-4/5 rounded-2xl bg-recipeGray-lightest px-4 pb-7 pt-2'>
+          <p className='mb-1 text-3xl font-bold'>
+            {tCreateRecipe('addRecipeIngredients')}
+          </p>
 
           {createRecipeState.ingredientsSections.map(
             (section, sectionIndex) => (
               <div
-                className='bg-recipeGray-default group relative mb-3 rounded-xl px-4 py-2'
+                className='group relative mb-3 rounded-xl bg-recipeGray-default px-4 py-2'
                 key={sectionIndex}
               >
                 <p className='mb-1 text-xl font-semibold opacity-80'>
                   {section.header.length > 0
                     ? section.header
-                    : t('sectionName')}
+                    : tCreateRecipe('sectionName')}
                 </p>
 
                 <section className='flex flex-row items-center justify-between'>
@@ -149,10 +163,30 @@ const CreateRecipe: React.FC = () => {
                     </p>
                     <Dropdown
                       isSearchable={true}
-                      placeholder={'Mitzrachim'}
-                      items={[]}
-                      onChange={() => {}}
-                      size={DropdownSizes.SMALL}
+                      placeholder={'Select ingredient'}
+                      items={sortedIngredientsList.map((ing) => ing.name)}
+                      onChange={(val) => {
+                        const ingObj = sortedIngredientsList.find(
+                          (ing) => ing.name === val
+                        );
+                        console.log(
+                          'ingredientId',
+                          val,
+                          ingObj,
+                          sortedIngredientsList
+                        );
+                        if (ingObj) {
+                          createRecipeDispatch({
+                            type: createRecipeAction.UPDATE_NEW_INGREDIENT_FIELD,
+                            payload: {
+                              sectionIndex: section.index,
+                              field: 'ingredientId',
+                              value: ingObj._id,
+                            },
+                          });
+                        }
+                      }}
+                      size={DropdownSizes.DEFAULT}
                     />
                   </section>
                   <section className='w-[25%]'>
@@ -162,9 +196,23 @@ const CreateRecipe: React.FC = () => {
                     <Dropdown
                       isSearchable={true}
                       placeholder={'Units'}
-                      items={[]}
-                      onChange={() => {}}
-                      size={DropdownSizes.SMALL}
+                      items={
+                        Object.values(Units)
+                          .map((unit) => tUnits(unit))
+                          .sort((a, b) => a.localeCompare(b)) || []
+                      }
+                      onChange={(val) => {
+                        console.log(val);
+                        createRecipeDispatch({
+                          type: createRecipeAction.UPDATE_NEW_INGREDIENT_FIELD,
+                          payload: {
+                            sectionIndex: section.index,
+                            field: 'unit',
+                            value: val,
+                          },
+                        });
+                      }}
+                      size={DropdownSizes.DEFAULT}
                     />
                   </section>
                   <section className='w-[20%]'>
@@ -198,9 +246,17 @@ const CreateRecipe: React.FC = () => {
                   <Chips
                     values={section.quantifiedIngredients.map(
                       (ingredient) =>
-                        `${ingredient.quantity} ${ingredient.unit} ${ingredient.ingredientId}`
+                        `${ingredient.quantity} ${ingredient.unit} ${sortedIngredientsList.find((ing) => ing._id === ingredient.ingredientId)?.name}`
                     )}
-                    onRemove={() => {}}
+                    onRemove={(index) => {
+                      createRecipeDispatch({
+                        type: createRecipeAction.REMOVE_INGREDIENT_FROM_SECTION,
+                        payload: {
+                          sectionIndex: section.index,
+                          ingredientIndex: index,
+                        },
+                      });
+                    }}
                   />
                 </section>
                 <button
@@ -224,6 +280,36 @@ const CreateRecipe: React.FC = () => {
                 type: createRecipeAction.ADD_INGREDIENT_SECTION,
               });
             }}
+            className='absolute bottom-1 left-1/2 -translate-x-1/2 transform'
+          />
+        </section>
+
+        <section className='relative mr-4 h-fit w-4/5 rounded-2xl bg-recipeGray-lightest px-4 pb-7 pt-2'>
+          <p className='mb-1 text-3xl font-bold'>
+            {tCreateRecipe('addRecipeSteps')}
+          </p>
+          <div className='group relative mb-3 rounded-xl bg-recipeGray-default px-4 py-2'>
+            <p className='mb-1 text-xl font-semibold opacity-80'>
+              {tCreateRecipe('sectionName')}
+            </p>
+            <section className='flex flex-row items-center justify-between'>
+              <section className='w-[40%]'>
+                <ol type='A'>
+                  <li>
+                    <input
+                      type='text'
+                      className='w-full border-b-black bg-transparent px-3 py-2 focus:outline-none border-2'
+                    />
+                  </li>
+                  <li>abc</li>
+                  <li>abc</li>
+                </ol>
+              </section>
+            </section>
+          </div>
+
+          <PlusButton
+            onClick={() => {}}
             className='absolute bottom-1 left-1/2 -translate-x-1/2 transform'
           />
         </section>

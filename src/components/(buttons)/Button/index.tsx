47199fc3b001f7d7ef6bd4319141432824c82fd7
tomershoham10 <tomershoham10@gmail.com>
2024-09-24 19:03:25 +0300

    style: removed height, added padding

--- src/components/(buttons)/Button/index.tsx
+++ src/components/(buttons)/Button/index.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<ButtonProps> = (props) => {
   const { label, className, onClick } = props;
   return (
     <button
-      className={`h-12 w-24 rounded-lg bg-recipeBlue-default text-xl font-bold text-recipeGray-lightest ${className}`}
+      className={`w-24 rounded-lg bg-recipeBlue-default py-[0.4rem] text-xl font-bold text-recipeGray-lightest ${className}`}
       onClick={onClick}
     >
       {label}

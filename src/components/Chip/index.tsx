6f54d59998596103f59f3d21a3d221c1ada350cf
tomershoham10 <tomershoham10@gmail.com>
2024-09-30 21:59:03 +0300

    feat: editMode prop

--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -3,21 +3,24 @@ import { FaXmark } from 'react-icons/fa6';
 
 interface ChipsProps {
   values: string[];
-  onRemove: (index: number) => void;
+  editMode: boolean;
+  onRemove?: (index: number) => void;
 }
 
 const Chips: React.FC<ChipsProps> = (props) => {
-  const { values, onRemove } = props;
+  const { values, editMode, onRemove } = props;
   return (
     <>
       {values.map((label, index) => (
         <div
           key={index}
-          className='flex w-fit cursor-default flex-row gap-2 rounded-3xl bg-recipeBlue-default py-1 pl-3 pr-2 text-lg text-recipeGray-lightest'
+          className={`flex w-fit cursor-default flex-row gap-2 rounded-3xl bg-recipeBlue-default py-1 pl-3 text-lg text-recipeGray-lightest ${editMode && onRemove ? 'pr-2' : 'pr-3'}`}
         >
-          <button className='cursor-pointer' onClick={() => onRemove(index)}>
-            <FaXmark />
-          </button>
+          {editMode && onRemove && (
+            <button className='cursor-pointer' onClick={() => onRemove(index)}>
+              <FaXmark />
+            </button>
+          )}
           {label}
         </div>
       ))}

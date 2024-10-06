5fade8d19217d7783bacf64ef38a75c650e1108f
tomershoham10 <tomershoham10@gmail.com>
2024-10-06 09:41:55 +0300

    feat: like button (not functional)

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import pRetry from 'p-retry';
 import React, { useCallback, useEffect, useState } from 'react';
 import Chips from '../Chip';
+import { FaRegHeart } from 'react-icons/fa6';
 
 interface RecipeCardProps {
   recipe: RecipeType;
@@ -37,28 +38,35 @@ const RecipeCard: React.FC<RecipeCardProps> = (props) => {
   }, [getFile, recipe]);
   return (
     <div
-      className='h-[370px] w-[375px] cursor-pointer rounded-xl bg-recipeGray-default px-3 py-2 shadow-xl transition-all duration-200 ease-in-out hover:scale-[102.5%]'
+      className='relative h-[370px] w-[375px] cursor-pointer rounded-xl bg-recipeGray-default px-3 py-2 shadow-xl transition-all duration-200 ease-in-out hover:scale-[102.5%]'
       onClick={() => onClick(recipe._id)}
     >
+      <button
+        className='absolute left-5 top-6 rounded-full bg-recipeGray-lightest p-[0.375rem] text-base text-recipeBlue-darker'
+        onClick={() => {}}
+      >
+        <FaRegHeart />
+      </button>
+
       {recipe.picture && url && (
         <Image
           src={url}
-          alt='Blob image'
+          alt='recipimage'
           width={350}
           height={350}
           className='rounded-3xl py-2'
         />
       )}
-      <div>
+      <div className='mb-[3px] flex flex-col gap-[3px]'>
         <h2 className='text-xl font-bold'>{recipe.name}</h2>
-        <p>{recipe.description}</p>
+        <p className='line-clamp-2 font-semibold'>{recipe.description}</p>
       </div>
       <section className='flex flex-wrap gap-2 py-1'>
-        <Chips
-          values={[...recipe.categories, recipe.difficultyLevel]}
-          editMode={false}
-        />
+        <Chips values={[...recipe.categories]} editMode={false} />
       </section>
+      <span className='absolute bottom-2 left-1/2 -translate-x-1/2 font-semibold opacity-90'>
+        - {recipe.difficultyLevel} | 1.5 hours -
+      </span>
     </div>
   );
 };

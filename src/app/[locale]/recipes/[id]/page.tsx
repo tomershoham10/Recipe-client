cac4b598776d4e32a3842446985285aa5b231143
tomershoham10 <tomershoham10@gmail.com>
2024-10-06 10:44:54 +0300

    feat: fetchIngredients + styling the page

--- src/app/[locale]/recipes/[id]/page.tsx
+++ src/app/[locale]/recipes/[id]/page.tsx
@@ -1,14 +1,14 @@
-// type Params = {
-//   params: {
-//     id: string;
-//   };
-// };
-
-import { BucketsNames, getFileByName } from '@/app/API/files-service/functions';
-import { getRecipeById } from '@/app/API/recipe-service/recipes/functions';
+import pRetry from 'p-retry';
 import Image from 'next/image';
+import Chips from '@/components/Chip';
 
-async function fetchRecipe(id: string): Promise<RecipeType | null> {
+import { BucketsNames } from '@/app/API/files-service/functions';
+import { getRecipeById } from '@/app/API/recipe-service/recipes/functions';
+import { getAllIngredients } from '@/app/API/recipe-service/ingredients/functions';
+
+// #region fetching data from server
+
+const fetchRecipe = async (id: string): Promise<RecipeType | null> => {
   try {
     console.log('fetchRecipe', id);
     const response = await getRecipeById(id);
@@ -16,15 +16,42 @@ async function fetchRecipe(id: string): Promise<RecipeType | null> {
   } catch (error) {
     throw new Error(`error getting recipe by id - ${error}`);
   }
-}
+};
+
+const fetchIngredients = async (): Promise<IngredientType[] | null> => {
+  try {
+    const response = await pRetry(getAllIngredients, {
+      retries: 5,
+      onFailedAttempt: (error) =>
+        console.error(
+          `fetchIngredients Attempt ${error.attemptNumber} failed. Retrying...`
+        ),
+    });
+    return response;
+  } catch (error) {
+    throw new Error(`error fetchIngredients - ${error}`);
+  }
+};
+
 const getFile = async (recipeId: string, objectName: string): Promise<Blob> => {
   try {
     const encodedObjectName = encodeURIComponent(objectName);
-    const response = await fetch(
-      `http://localhost:4002/api/files/getFileByName/${BucketsNames.RECIPES}/${recipeId}/${encodedObjectName}`,
+
+    const response = await pRetry(
+      () =>
+        fetch(
+          `http://localhost:4002/api/files/getFileByName/${BucketsNames.RECIPES}/${recipeId}/${encodedObjectName}`,
+          {
+            method: 'GET',
+            credentials: 'include',
+          }
+        ),
       {
-        method: 'GET',
-        credentials: 'include',
+        retries: 5,
+        onFailedAttempt: (error) =>
+          console.error(
+            `getFile Attempt ${error.attemptNumber} failed. Retrying...`
+          ),
       }
     );
 
@@ -35,70 +62,106 @@ const getFile = async (recipeId: string, objectName: string): Promise<Blob> => {
   }
 };
 
+// #endregion
+
 const Recipes = async ({ params }: { params: { id: string } }) => {
-  const recipe = await fetchRecipe(params.id);
+  const [recipe, ingredientsList] = await Promise.all([
+    fetchRecipe(params.id),
+    fetchIngredients(),
+  ]);
   if (!recipe) {
     return <p>recipe not found</p>;
   }
+  if (!ingredientsList) {
+    return <p>server error</p>;
+  }
+
   const blob = await getFile(recipe._id, recipe.picture);
   const buffer = await blob.arrayBuffer();
   const base64 = Buffer.from(buffer).toString('base64');
   const src = `data:${blob.type};base64,${base64}`;
+
   return (
-    <section className='overflow-y-auto'>
+    <section className='h-full w-full overflow-y-auto'>
       {recipe && (
-        <div>
-          {blob && (
-            <div className='relative h-72 w-full'>
-              <Image src={src} alt='File' layout='fill' objectFit='cover' />
-            </div>
-          )}
+        <div className='h-full w-full'>
+          <header className='flex w-full flex-col items-center justify-center gap-3 pt-12'>
+            <h1 className='text-5xl font-bold'>{recipe.name}</h1>
+            <h2 className='text-4xl'>{recipe.description}</h2>
 
-          <h1>{recipe.name}</h1>
-          <h2>{recipe.description}</h2>
-          <h2>
-            {recipe.categories.map((cat) => (
-              <p key={cat}>{cat}</p>
-            ))}
-          </h2>
-          <h3>
-            ingredients
-            {recipe.ingredientsSections &&
-              recipe.ingredientsSections.map((ingSection) => (
-                <section key={ingSection.index}>
-                  <p>{ingSection.header}</p>
-                  <section>
-                    {ingSection.quantifiedIngredients.map((quntIng) => (
-                      <p key={quntIng.index}>
-                        {quntIng.index}
-                        <br />
-                        {quntIng.ingredientId}
-                      </p>
-                    ))}
-                  </section>
-                </section>
-              ))}
-          </h3>
+            <Chips values={recipe.categories} editMode={false} />
+            {blob && (
+              <div className='relative ml-2 h-fit w-fit overflow-hidden transition-all duration-200 ease-in'>
+                <Image
+                  src={src}
+                  alt='recipe image'
+                  className='rounded-xl py-2'
+                  width={900}
+                  height={1600}
+                />
+              </div>
+            )}
+          </header>
 
-          <h3>
-            steps
-            {recipe.stepsSections &&
-              recipe.stepsSections.map((stepSection) => (
-                <section key={stepSection.index}>
-                  <p>{stepSection.header}</p>
-                  <section>
-                    {stepSection.steps &&
-                      stepSection.steps.map((step) => (
-                        <p key={step.index}>
-                          {step.index}
-                          <br />
-                          {step.info}
-                        </p>
+          <section className='px-6'>
+            <>
+              <p className='text-3xl font-semibold'>ingredients</p>
+              {recipe.ingredientsSections &&
+                recipe.ingredientsSections.map((ingSection) => (
+                  <section key={ingSection.index} className='mb-3'>
+                    <p className='text-xl font-semibold'>{ingSection.header}</p>
+                    <section className='text-lg'>
+                      {ingSection.quantifiedIngredients.map((quntIng) => (
+                        <span
+                          key={quntIng.index}
+                          className='flex flex-row gap-1'
+                        >
+                          {ingredientsList &&
+                            ingredientsList.find(
+                              (ing) => ing._id === quntIng.ingredientId
+                            ) && (
+                              <>
+                                <p>{quntIng.quantity}</p>
+                                <p>{quntIng.unit}</p>
+                                <p>
+                                  {
+                                    ingredientsList.find(
+                                      (ing) => ing._id === quntIng.ingredientId
+                                    )!.name
+                                  }
+                                </p>
+                              </>
+                            )}
+                        </span>
                       ))}
+                    </section>
                   </section>
-                </section>
-              ))}
-          </h3>
+                ))}
+            </>
+            <>
+              <p className='text-3xl font-semibold'>steps</p>
+
+              {recipe.stepsSections &&
+                recipe.stepsSections.map((stepSection) => (
+                  <section key={stepSection.index} className='mb-3'>
+                    <p className='text-xl font-semibold'>
+                      {stepSection.header}
+                    </p>
+                    <section className='text-lg'>
+                      {stepSection.steps &&
+                        stepSection.steps.map((step) => (
+                          <span
+                            key={step.index}
+                            className='flex flex-row gap-1'
+                          >
+                            {step.info}
+                          </span>
+                        ))}
+                    </section>
+                  </section>
+                ))}
+            </>
+          </section>
         </div>
       )}
     </section>

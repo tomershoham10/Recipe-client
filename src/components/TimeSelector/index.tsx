6398f1684cf8fde43aaf9d93643045255d1f43b1
tomershoham10 <tomershoham10@gmail.com>
2024-10-09 16:12:31 +0300

    feat: TimeSelector

---
+++ src/components/TimeSelector/index.tsx
@@ -0,0 +1,74 @@
+import { useState, useCallback } from 'react';
+import { useTranslations } from 'next-intl';
+import BorderedInput from '@/components/(inputs)/BorderedInput';
+
+export interface TimeSelectorProps {
+  onTimeSelect?: (days: number, hours: number, minutes: number) => void;
+}
+
+const TimeSelector: React.FC<TimeSelectorProps> = ({ onTimeSelect }) => {
+  const t = useTranslations('createRecipe');
+
+  // State to manage the input values for days, hours, and minutes
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+
+  // Function to handle time input changes and propagate the selected time upwards
+  const handleTimeChange = useCallback(
+    (unit: 'days' | 'hours' | 'minutes', value: string) => {
+      const numericValue = Math.max(0, parseInt(value, 10) || 0);
+      if (unit === 'days') setDays(numericValue);
+      if (unit === 'hours') setHours(Math.min(23, numericValue));
+      if (unit === 'minutes') setMinutes(Math.min(59, numericValue));
+
+      if (onTimeSelect) {
+        onTimeSelect(
+          unit === 'days' ? numericValue : days,
+          unit === 'hours' ? numericValue : hours,
+          unit === 'minutes' ? numericValue : minutes
+        );
+      }
+    },
+    [days, hours, minutes, onTimeSelect]
+  );
+
+  return (
+    <section className='flex w-full flex-col gap-2'>
+      <p className='mb-1 text-lg font-semibold opacity-60'>
+        {t('recipeDuration')}
+      </p>
+      <section className='flex w-full items-center gap-4'>
+        <div className='flex flex-col'>
+          <label className='text-sm'>{t('days')}</label>
+          <BorderedInput
+            type='number'
+            value={days}
+            onChange={(e) => handleTimeChange('days', e.target.value)}
+            className='w-16 text-center'
+          />
+        </div>
+        <div className='flex flex-col'>
+          <label className='text-sm'>{t('hours')}</label>
+          <BorderedInput
+            type='number'
+            value={hours}
+            onChange={(e) => handleTimeChange('hours', e.target.value)}
+            className='w-16 text-center'
+          />
+        </div>
+        <div className='flex flex-col'>
+          <label className='text-sm'>{t('minutes')}</label>
+          <BorderedInput
+            type='number'
+            value={minutes}
+            onChange={(e) => handleTimeChange('minutes', e.target.value)}
+            className='w-16 text-center'
+          />
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default TimeSelector;

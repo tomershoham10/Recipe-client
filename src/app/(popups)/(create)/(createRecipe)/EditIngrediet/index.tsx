44de0a6736f8b1af7648194a57a62086695d4967
tomershoham10 <tomershoham10@gmail.com>
2024-10-13 13:54:36 +0300

    feat: updateIngredient

--- src/app/(popups)/(create)/(createRecipe)/EditIngrediet/index.tsx
+++ src/app/(popups)/(create)/(createRecipe)/EditIngrediet/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslations } from 'next-intl';
 
 import Button from '@/components/(buttons)/Button';
@@ -8,6 +8,7 @@ import { PopupsTypes } from '@/app/store/stores/usePopupStore';
 import {
   CreateRecipeAction,
   CreateRecipeType,
+  RecipeActionsList,
 } from '@/reducers/createRecipeReducer';
 import Dropdown, { DropdownSizes } from '@/components/Dropdown/page';
 import { Units } from '@/app/API/recipe-service/ingredients/functions';
@@ -36,10 +37,16 @@ const CreateRecipeEditIngredient: React.FC<CreateRecipeEditIngredientProps> = (
   const t = useTranslations('createRecipe.editIngrediet');
   const tButtons = useTranslations('buttons');
 
+  const [updatedIngredient, setUpdatedIngredient] =
+    useState<QuantifiedIngredient | null>(null);
+
   const quantifiedIngredient = useMemo(() => {
     if (sectionIndex !== null && ingredientIndex !== null) {
-      return createRecipeState.ingredientsSections[sectionIndex]
-        .quantifiedIngredients[ingredientIndex];
+      const quantIng =
+        createRecipeState.ingredientsSections[sectionIndex]
+          .quantifiedIngredients[ingredientIndex];
+      setUpdatedIngredient(quantIng);
+      return quantIng;
     } else return null;
   }, [createRecipeState, sectionIndex, ingredientIndex]);
 
@@ -51,6 +58,35 @@ const CreateRecipeEditIngredient: React.FC<CreateRecipeEditIngredientProps> = (
       return ingOnj || null;
     } else return null;
   }, [ingredientsList, quantifiedIngredient]);
+
+  // useEffect(() => {
+  //   console.log('updatedIngredient', updatedIngredient);
+  // }, [updatedIngredient]);
+
+  const updateIngredient = useCallback(() => {
+    if (
+      sectionIndex !== null &&
+      ingredientIndex !== null &&
+      updatedIngredient !== null
+    ) {
+      createRecipeDispatch({
+        type: RecipeActionsList.UPDATE_INGREDIENT,
+        payload: {
+          sectionIndex: sectionIndex,
+          ingredientIndex: ingredientIndex,
+          updatedIngredient: updatedIngredient,
+        },
+      });
+      console.log('updateIngredient func', updatedIngredient);
+      onClose();
+    }
+  }, [
+    createRecipeDispatch,
+    ingredientIndex,
+    onClose,
+    sectionIndex,
+    updatedIngredient,
+  ]);
 
   if (ingredientObject === null) {
     // alert('server error');
@@ -68,29 +104,63 @@ const CreateRecipeEditIngredient: React.FC<CreateRecipeEditIngredientProps> = (
         <section className='flex h-full flex-row items-center gap-3'>
           <Input
             type={InputTypes.NUMBER}
-            value={10}
+            value={updatedIngredient?.quantity}
             onChange={(quantity) => {
               console.log(quantity);
+              setUpdatedIngredient((prev) => {
+                if (prev === null) return prev;
+
+                return {
+                  ...prev,
+                  quantity: Number(quantity),
+                };
+              });
             }}
             className='max-w-[5rem]'
           />
           <Dropdown
             isSearchable={true}
-            value={undefined}
+            value={updatedIngredient?.unit}
             placeholder={t('units')}
             items={
               Object.values(Units).sort((a, b) => a.localeCompare(b)) || []
             }
             onChange={(val) => {
-              console.log(val);
+              console.log('unit', val);
+              setUpdatedIngredient((prev) => {
+                if (prev === null) return prev;
+
+                return {
+                  ...prev,
+                  unit: val as Units,
+                };
+              });
             }}
             size={DropdownSizes.DEFAULT}
+          />
+
+          <Input
+            type={InputTypes.TEXT}
+            placeholder={t('addComment')}
+            value={updatedIngredient?.comment}
+            onChange={(comment) => {
+              console.log('comment', comment);
+              setUpdatedIngredient((prev) => {
+                if (prev === null) return prev;
+
+                return {
+                  ...prev,
+                  comment: String(comment),
+                };
+              });
+            }}
+            className='w-[40%]'
           />
         </section>
 
         <Button
           label={tButtons('save')}
-          onClick={() => {}}
+          onClick={updateIngredient}
           className=''
         />
       </div>

ed70b5bbe8f30d4a97b4d184ae9f5ef88a31df7d
tomershoham10 <tomershoham10@gmail.com>
2024-10-13 10:12:41 +0300

    feat: edit option prop

--- src/components/Chip/index.tsx
+++ src/components/Chip/index.tsx
@@ -1,21 +1,22 @@
 'use client';
 import { ReactNode } from 'react';
-import { FaXmark } from 'react-icons/fa6';
+import { FaPen, FaXmark } from 'react-icons/fa6';
 
 interface ChipsProps {
   values: string[] | ReactNode[];
   editMode: boolean;
+  onEdit?: (index: number) => void;
   onRemove?: (index: number) => void;
 }
 
 const Chips: React.FC<ChipsProps> = (props) => {
-  const { values, editMode, onRemove } = props;
+  const { values, editMode, onEdit, onRemove } = props;
   return (
     <>
       {values.map((label, index) => (
         <div
           key={index}
-          className={`flex w-fit cursor-default flex-row gap-2 rounded-3xl bg-recipeBlue-default py-1 pl-3 text-lg text-recipeGray-lightest ${editMode && onRemove ? 'pr-2' : 'pr-3'}`}
+          className={`flex w-fit cursor-default select-none flex-row gap-2 rounded-3xl bg-recipeBlue-default py-1 pl-3 text-lg text-recipeGray-lightest ${editMode && onRemove ? 'pr-2' : 'pr-3'}`}
         >
           {editMode && onRemove && (
             <button className='cursor-pointer' onClick={() => onRemove(index)}>
@@ -23,6 +24,16 @@ const Chips: React.FC<ChipsProps> = (props) => {
             </button>
           )}
           {label}
+          {editMode && onEdit && (
+            <button
+              className='cursor-pointer text-sm'
+              onClick={() => {
+                onEdit(index);
+              }}
+            >
+              <FaPen />
+            </button>
+          )}
         </div>
       ))}
     </>
